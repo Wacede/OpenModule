6238d4a8c9416df0a66cb2cfc19a9e0055a5a61a
Alberto Quiles <alberto.quil3s@gmail.com>
2020-05-14 09:00:06 +0200

    Add log command

--- main.ts
+++ main.ts
@@ -2,3 +2,4 @@ export const foo = () => 'Hello public land!';
 
 export const sayHi = (name: string) => `Hello ${name} lander!`;
 export const ping = () => `...pong`;
+export const log = () => console.log("Logged : D"); 
